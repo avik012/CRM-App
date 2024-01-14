50e498eb30c31bb6706ba71b9a6836e9f0723d36
avik012 <Avinashkumawat12@gmail.com>
2024-01-15 04:21:24 +0530

    CRM completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './components/User/Home.js';
+import LoginSignUp from './components/User/LoginSignUp';
+import Profile from './components/User/Profile';
+import { clearErrors, loadUser } from './actions/userAction';
+import store from './store'
+import { useEffect } from 'react';
+import ProtectedRoute from './components/Route/ProtectedRoute.js'
+import { useSelector } from 'react-redux';
+import Products from './components/Product/Products.js';
+import ProductDetails from './components/Product/ProductDetails.js';
+import UpdateProduct from './components/Product/UpdateProduct.js';
+import NewProduct from './components/Product/NewProduct.js';
+import Header from './components/Layout/Header/Header.js';
+import Dashboard from './components/dashboard/Dashboard.js';
 
 function App() {
+
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+    store.dispatch(clearErrors());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header />
+      <Routes>
+      <Route exact path='/' Component={Home}  />
+      <Route exact path='/login' Component={LoginSignUp}  />
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/> }>
+          <Route path="/account" element= {<Profile />} />
+      </Route>
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/> }>
+          <Route path="/products" element= {<Products />} />
+      </Route>
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/> }>
+          <Route path="/products/:id" element= {<ProductDetails />} />
+      </Route>
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/> }>
+          <Route path="/product/update/:id" element= {<UpdateProduct />} />
+      </Route>
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/> }>
+          <Route path="/product/create" element= {<NewProduct />} />
+      </Route>
+
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/> }>
+          <Route path="/dashboard" element= {<Dashboard />} />
+        </Route>
+
+
+      </Routes>
+    </Router>
+    
+    </>
   );
 }
 
